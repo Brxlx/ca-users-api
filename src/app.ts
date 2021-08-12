3c575d2c7a043d7da2e53fd08a2f17485beb013b
Bruno Santos <bruno@pop-os.localdomain>
2021-08-12 19:37:21 -0300

    chore: set up basic project run

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+import "dotenv/config";
+const app = fastify();
+
+const APP_PORT = process.env.APP_PORT || 5000;
+app.listen(APP_PORT, ()=>{
+  console.log(`App running on port ${APP_PORT}`)
+})
+
+
+export {app};
