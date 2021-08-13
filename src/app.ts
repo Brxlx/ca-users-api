a6037b2dc99bb6834f84bb25bfd5b04596fbc340
Bruno Santos <brcesar19@gmail.com>
2021-08-13 09:45:11 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import 'dotenv/config';
 import fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
+// import fastifyExpress from 'fastify-express';
 
 const app = fastify();
+
+// cors configuration
+app.register(fastifyCors, {});
 
 export { app };
