c52459da764a200e69383c08bc65bf20fb8d315e
Bruno Santos <brcesar19@gmail.com>
2021-08-13 10:00:25 -0300

    feat: cors config and debug

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import 'dotenv/config';
 import fastify from 'fastify';
-import fastifyCors from 'fastify-cors';
+import cors from 'fastify-cors';
 // import fastifyExpress from 'fastify-express';
 
 const app = fastify();
 
 // cors configuration
-app.register(fastifyCors, {});
+app.register(cors, {
+  origin: (org, cb) => {
+    // org = '192.168.0.0';
+    if (/localhost/.test(org) || /undefined/.test(org)) {
+      cb(null, true);
+    }
+    setTimeout(() => {
+      cb(new Error('Not allowed origin'), false);
+    }, 750);
+  },
+});
 
 export { app };
