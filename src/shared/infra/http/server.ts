6c09c24ca343540de20b09746245706d6f022066
Bruno Santos <brcesar19@gmail.com>
2021-08-17 09:28:46 -0300

    chore: log all routes on app start

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,5 +3,6 @@ import { app } from './app';
 const APP_PORT = Number(process.env.APP_PORT) || 5000;
 
 app.listen(APP_PORT, () => {
+  console.log(app.printRoutes());
   console.log(`App running on port ${APP_PORT}`);
 });
