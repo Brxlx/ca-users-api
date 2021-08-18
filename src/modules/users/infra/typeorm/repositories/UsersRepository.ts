54ef909ddb12061fb0d3ad0891ec4bc2d2bbe637
Bruno Santos <brcesar19@gmail.com>
2021-08-18 12:44:56 -0300

    refactor(UsersRepository): getRepository on constructor

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,10 +1,14 @@
-import { getRepository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import { IUsersRepository } from '../../../repositories/contracts/IUsersRepository';
 import { User } from '../entities/Users';
 
 class UsersRepository implements IUsersRepository {
-  private usersRepository = getRepository(User);
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getRepository(User);
+  }
 
   public async findAll(): Promise<User[]> {
     return this.usersRepository.find();
