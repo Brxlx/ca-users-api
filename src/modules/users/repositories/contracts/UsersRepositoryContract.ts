02ca15a9f8535e1dd56d30b2814e1ac04f05ff98
Bruno Santos <brcesar19@gmail.com>
2021-09-01 17:44:50 -0300

    fix: calling repository via contract

--- src/modules/users/repositories/contracts/UsersRepositoryContract.ts
+++ src/modules/users/repositories/contracts/UsersRepositoryContract.ts
@@ -1,12 +1,22 @@
+import { UpdateResult } from 'typeorm';
+
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { User } from '../../infra/typeorm/entities/Users';
 
 abstract class UsersRepositoryContract {
   public findAll: () => Promise<User[]>;
 
-  public create: (data: ICreateUserDTO) => Promise<User>;
+  public findById: (id: string) => Promise<User>;
 
   public findByNickname: (nickname: string) => Promise<User>;
+
+  public create: (data: ICreateUserDTO) => Promise<User>;
+
+  public updateLastnameAndBio: (id: string, lastName: string, bio: string) => Promise<UpdateResult>;
+
+  public updateNickname: (id: string, nickname: string) => Promise<UpdateResult>;
+
+  public delete: (id: string) => Promise<void>;
 }
 
 export { UsersRepositoryContract };
