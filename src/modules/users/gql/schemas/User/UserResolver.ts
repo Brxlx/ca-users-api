43e1ae45d110bb1090b84a44a09ede2e5cf45b3b
Bruno Santos <brcesar19@gmail.com>
2021-10-26 17:33:41 -0300

    feat(users): gql mutation for create a new user

--- src/modules/users/gql/schemas/User/UserResolver.ts
+++ src/modules/users/gql/schemas/User/UserResolver.ts
@@ -1,4 +1,6 @@
+import { ICreateUserDTO } from 'src/modules/users/dtos/ICreateUserDTO';
 import { User } from 'src/modules/users/infra/typeorm/entities/Users';
+import { CreateUserUseCase } from 'src/modules/users/useCases/CreateUser/CreateUserUseCase';
 import { GetUserUseCase } from 'src/modules/users/useCases/GetUser/GetUserUseCase';
 import { container } from 'tsyringe';
 
@@ -12,11 +14,16 @@ const usersResolver = {
     },
 
     async getUser(_: any, { nickname }: User) {
-      const getUser = container.resolve(GetUserUseCase);
-      return getUser.execute(nickname);
+      const getUserUseCase = container.resolve(GetUserUseCase);
+      return getUserUseCase.execute(nickname);
     },
   },
-  // Mutation: {},
+  Mutation: {
+    async createUser(_: any, { firstName, lastName, nickname, address, bio }: ICreateUserDTO) {
+      const createUserUseCase = container.resolve(CreateUserUseCase);
+      return createUserUseCase.execute({ firstName, lastName, nickname, address, bio });
+    },
+  },
 };
 
 export default usersResolver;
